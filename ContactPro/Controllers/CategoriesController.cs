43779793139c9e5f8a491ef124bd4f9c8e54dc51
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-18 23:56:45 -0600

    secure contacts

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ContactPro.Data;
 using ContactPro.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ContactPro.Controllers
 {
