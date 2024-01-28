f1d5cdca026ce731655fa2e24462c02c40a610a9
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2024-01-28 13:17:00 -0600

    finish Interface categories

---
+++ ContactPro/Services/AddressBookService.cs
@@ -0,0 +1,12 @@
+﻿using ContactPro.Data;
+using ContactPro.Models;
+using Microsoft.EntityFrameworkCore;
+using ContactPro.Services.Interfaces;
+
+namespace ContactPro.Services
+{
+	public class AddressBookService
+	{
+		public class AddressBookService
+	}
+}
